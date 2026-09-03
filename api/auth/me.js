d5434e516493f9f9ae43319851b548fe6ce118ce
ROHYOUNSOOK <host@daplan.com>
2026-09-03 19:34:57 +0900

    Delete api directory

--- api/auth/me.js
+++
@@ -1,9 +0,0 @@
-import { getAuth, publicUser, sessionCookie, sessionFromUser } from '../../lib/auth.js';
-
-export default async function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).json({ error: 'GET only' });
-  const auth = await getAuth(req);
-  if (!auth.user) return res.status(auth.status).json({ error: auth.error });
-  res.setHeader('Set-Cookie', sessionCookie(sessionFromUser(auth.user)));
-  res.status(200).json({ user: publicUser(auth.user) });
-}
